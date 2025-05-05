21fc769eaf8cdfcce33ebcab89adb1a05b88eb6c
VykoV <vallivictoria@gmail.com>
2025-05-05 16:49:56 -0300

    Ejercicio 4: Contar vocales en una palabra

---
+++ src/resolValliV/Ejercicio4.java
@@ -0,0 +1,31 @@
+package resolValliV;
+
+import java.util.Scanner;
+
+public class Ejercicio4 {
+    public static void main(String[] args){
+        /* Importo Java.Util.Scanner */
+        Scanner scanner = new Scanner(System.in);
+
+        /* Ingreso una palabra */
+        System.out.println("Ingrese una palabra");
+        String palabra = scanner.next().toLowerCase();
+
+        /* Cantidad de letras */
+        int letras = palabra.length();
+
+        System.out.println("La palabra contiene " + letras + " letras");
+
+        /* Declaro la variable vocal */
+        int vocal = 0;
+
+        /* Cuento vocales */
+        for (int i = 0; i < letras; i++) {
+            if(palabra.charAt(i)=='a' || palabra.charAt(i)=='e' || palabra.charAt(i)=='i' || palabra.charAt(i)=='o' || palabra.charAt(i)=='u'){
+                vocal++;
+            }
+        }
+        /*Muestro el resultado*/
+        System.out.println("La cantidad de vocales que tiene la palabra '" + palabra + "' es: " + vocal);
+    }
+}
