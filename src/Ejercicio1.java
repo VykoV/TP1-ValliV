1f03a06f52aa6346f3f6ec67a42f7c5ab47242e2
VykoV <vallivictoria@gmail.com>
2025-05-05 12:02:29 -0300

    Ejercicio 1 - Programa Saludador

---
+++ src/Ejercicio1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        /*Declaro Java.util.Scanner*/
+        Scanner scanner=new Scanner(System.in);
+
+        /* Ingreso del nombre*/
+        System.out.println("Ingrese su nombre");
+        String nombre= scanner.nextLine();
+
+        /* Salida del nombre*/
+        System.out.println("¡Hola " + nombre + "!");
+    }
+}
