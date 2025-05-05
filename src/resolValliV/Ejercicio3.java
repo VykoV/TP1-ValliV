253501b6cc1a273cbb3582671b6a5996e1830196
VykoV <vallivictoria@gmail.com>
2025-05-05 15:44:31 -0300

    Ejercicio 3: Tabla de multiplicar

---
+++ src/resolValliV/Ejercicio3.java
@@ -0,0 +1,24 @@
+package resolValliV;
+
+import java.util.Scanner;
+
+public class Ejercicio3 {
+    public static void main(String[] args){
+        /* Importo Java.Util.Scanner */
+        Scanner scanner = new Scanner(System.in);
+
+        /* Ingreso el dato */
+        System.out.println("Ingresar un Numero Entero");
+        int numero = scanner.nextInt();
+
+        /* Creo una variable contar */
+        int contar = 1;
+
+        /* Multiplico el numero del 1 al 10 */
+        for(int i=0; i<10; i++ ){
+            int multiplicar = numero * contar;
+            System.out.println(contar + " x " + numero + " = " + multiplicar);
+            contar++;
+        }
+    }
+}
