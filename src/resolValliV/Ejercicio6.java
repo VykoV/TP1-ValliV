7934dbaf4fcdaebe3229d38699ec89ba71b0bd34
VykoV <vallivictoria@gmail.com>
2025-05-06 18:18:51 -0300

    Ejercicio 6: Adivina el numero.

--- src/resolValliV/Ejercicio6.java
+++ src/resolValliV/Ejercicio6.java
@@ -1,4 +1,31 @@
 package resolValliV;
 
+import java.util.Scanner;
+
 public class Ejercicio6 {
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        double random = Math.round(Math.random() * 100);
+        /*System.out.println("random = " + random);*/
+
+        System.out.println("Ingresar un numero del 1 al 100");
+        int numero = 0;
+        int contador = 0;
+        do{
+             numero = scanner.nextInt();
+            if(numero==random){
+                contador ++;
+                System.out.println("¡Ha adivinado el numero!" + "\nEl numero: " + random + "\nCantidad de intentos:" + contador );
+
+            }else if(numero > random) {
+                contador ++;
+                System.out.println("El numero es mas bajo \nIntente nuevamente" );
+            } else if (numero < random) {
+                contador ++;
+                System.out.println("El numero es mas alto \nIntente nuevamente" );
+            }
+        }while (numero!=random);
+
+    }
 }
