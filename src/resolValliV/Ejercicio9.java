3e0f53428873c4ce2005b661050ced47bea8ff0c
VykoV <vallivictoria@gmail.com>
2025-05-07 00:40:02 -0300

    Ejercicio 9: Cuenta Palabras

---
+++ src/resolValliV/Ejercicio9.java
@@ -0,0 +1,22 @@
+package resolValliV;
+
+import java.util.Scanner;
+
+public class Ejercicio9 {
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese una oracion");
+        String oracion = scanner.nextLine();
+
+        String[] palabras = oracion.split("[\\s,¡!¿?.]+");
+
+        int contadorPalabras = 0;
+        for (int i = 0; i < palabras.length; i++) {
+            /*String palabra = palabras[i];
+            System.out.println(palabra);*/
+            contadorPalabras++;
+        }
+        System.out.println("En esta oracion hay " + contadorPalabras + " palabras");
+    }
+}
